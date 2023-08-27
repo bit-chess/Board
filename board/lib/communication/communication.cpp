91e5463fd2a3b5f837457a714ec5a5944578ec39
SageScroll18144 <ffss@cin.ufpe.br>
2023-08-27 02:11:29 -0300

    comunicação com a ESP

--- board/lib/communication/communication.cpp
+++ board/lib/communication/communication.cpp
@@ -2,8 +2,6 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 
-WiFiServer server(12345);
-
 void buildCommunication(void){
     WiFi.softAP(ssid, password);
     server.begin();
--- board/src/main.cpp
+++ board/src/main.cpp
@@ -1,9 +1,37 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
 #include <mux.h>
+#include <communication.h>
 
-void setup() {
+void setup(void) {
   Serial.begin(11520);
   buildSystem();
 }
 
-void loop() {}
+void loop(void) {
+  
+  WiFiClient client = server.available();
+
+  if(client) {
+      
+    Serial.println("Conexão estabelecida com o Desktop");
+    
+    while (client.connected()) {
+      if (client.available()) {
+        String message = client.readStringUntil('\n'); // movimento na notação do xadrez
+        Serial.println("Received: " + message);
+
+        // Process the message as needed
+
+        // envia
+        client.print("Message received: " + message);
+        client.flush();
+      }
+    }
+    
+    Serial.println("Conexão perdida");
+    
+  }
+
+}
