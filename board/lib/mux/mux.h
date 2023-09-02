c97bdcfc545a6a682d300a44be96b39d0b78657f
SageScroll18144 <ffss@cin.ufpe.br>
2023-09-02 10:53:13 -0300

    implementando teste de leitura do mux

--- board/lib/mux/mux.h
+++ board/lib/mux/mux.h
@@ -5,7 +5,7 @@
 
 const int seletor[QTD_MUX][QTD_MUX] = {{}}; //Seletores. Notação: LITTLE ENDIAN. {{S0s},{S1s},{S2s},{S3s}}
 const int which_mux[QTD_MUX] = {}; //conjunto de pinos de enable dos muxs
-const int SIG = 43; //Pino de leitura analógica
+const int SIG = 39; //Pino de leitura analógica
 
 void buildSystem(void);
 int returnPiece(int value_pin_seletor);
--- board/src/main.cpp
+++ board/src/main.cpp
@@ -1,46 +1,14 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <WiFiClient.h>
 #include <mux.h>
-
-// const char *ssid_ = "ESP32_P2P";
-// const char *password_ = "password";
-// WiFiServer server(12345);
+#include <piece.h>
+#include <image_board.h>
 
 void setup(void) {
   Serial.begin(115200);
   buildSystem();
-
-  //WiFi.softAP(ssid_, password_);
-  //server.begin();
 }
 
 void loop(void) {
-  
-  // WiFiClient client = server.available();
-
-  // if(client) {
-      
-  //   Serial.println("Conexão estabelecida com o Desktop");
-    
-  //   while (client.connected()) {
-  //     if (client.available()) {
-  //       String message = client.readStringUntil('\n'); // movimento na notação do xadrez
-  //       Serial.println("Received: " + message);
-
-  //       // Process the message as needed
-
-  //       // envia
-  //       client.print("Message received: " + message);
-  //       client.flush();
-  //     }
-  //   }
-    
-  //   Serial.println("Conexão perdida");
-
-  // }
-
-  Serial.println(analogRead(39));
+  Serial.println(returnPiece(0));
   delay(100);
-
 }
