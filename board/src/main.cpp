e80e3aa92c65bc196154a5ad094b14268c5aa136
SageScroll18144 <ffss@cin.ufpe.br>
2023-09-07 08:11:10 -0300

    Implementando representação matricial

--- board/src/main.cpp
+++ board/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <mux.h>
 #include <piece.h>
+#include <image_board.h>
 
 void setup() {
   Serial.begin(9600);
@@ -8,5 +9,5 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(analogToRepresentationPiece(returnPiece(0)));
+  char **board = updateImageBoard();
 }
