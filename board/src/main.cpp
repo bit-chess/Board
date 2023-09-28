a6803a63a01d24a656fff5e64c40e97b1dd28474
SageScroll18144 <ffss@cin.ufpe.br>
2023-09-28 16:50:52 -0300

    switch para ambos os sistemas

--- board/src/main.cpp
+++ board/src/main.cpp
@@ -8,9 +8,11 @@ void setup() {
   Serial.begin(115200);
   buildSystem();
   pinMode(13, OUTPUT);
+  pinMode(7, INPUT_PULLUP); //switch for which high level system
 }
 
 void loop() {
   digitalWrite(13, !digitalRead(13));
-  send_board_to_c(updateImageBoard(2));
+  if(!digitalRead(7)) send_board_to_c(updateImageBoard(2));
+  else send_board_to_java(updateImageBoard(2));
 }
